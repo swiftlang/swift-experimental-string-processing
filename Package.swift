f1a19b1ded8121cec7c69005668137a851d2f269
Michael Ilseman <michael.ilseman@gmail.com>
2021-09-29 17:02:48 -0600

    Rename package

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "MatchingEngine",
+    name: "swift-experimental-string-processing",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
