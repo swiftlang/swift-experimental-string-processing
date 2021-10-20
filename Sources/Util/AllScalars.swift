1e5f5b088df9b3f9968b285cc8689c4e7b901d7f
Nate Cook <natecook@apple.com>
2021-10-20 11:18:38 -0500

    Fix off by one error, improve names

--- Sources/Util/AllScalars.swift
+++ Sources/Util/AllScalars.swift
@@ -2,29 +2,29 @@ extension Unicode.Scalar {
   public struct AllScalars: RandomAccessCollection {
     // Unicode scalar values are in two discontiguous blocks:
     // 0...0xD7FF and 0xE000...0x10FFFF
-    private static var lowerSectionFirstIndex: Int { 0 }
-    private static var lowerSectionLastIndex: Int { 0xD7FF }
-    private static var upperSectionFirstIndex: Int { 0xE000 }
-    private static var upperSectionLastIndex: Int { 0x10FFFF }
+    internal static var lowerSectionFirstValue: Int { 0 }
+    internal static var lowerSectionLastValue: Int { 0xD7FF }
+    internal static var upperSectionFirstValue: Int { 0xE000 }
+    internal static var upperSectionLastValue: Int { 0x10FFFF }
     
-    private static var surrogateRangeCount: Int {
-      (upperSectionFirstIndex - lowerSectionLastIndex) + 1
+    internal static var surrogateRangeCount: Int {
+      (upperSectionFirstValue - lowerSectionLastValue) - 1
     }
     
-    private func toContiguous(_ i: Int) -> Int {
-      i >= Self.upperSectionFirstIndex
+    internal func toContiguous(_ i: Int) -> Int {
+      i >= Self.upperSectionFirstValue
         ? i - Self.surrogateRangeCount
         : i
     }
     
-    private func fromContiguous(_ i: Int) -> Int {
-      i > Self.lowerSectionLastIndex
+    internal func fromContiguous(_ i: Int) -> Int {
+      i > Self.lowerSectionLastValue
         ? i + Self.surrogateRangeCount
         : i
     }
     
     public var startIndex: Int { 0 }
-    public var endIndex: Int { toContiguous(Self.upperSectionLastIndex + 1) }
+    public var endIndex: Int { toContiguous(Self.upperSectionLastValue + 1) }
     
     public subscript(position: Int) -> Unicode.Scalar {
       Unicode.Scalar(UInt32(fromContiguous(position)))!
