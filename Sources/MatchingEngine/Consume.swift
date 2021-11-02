e1a8e679494157495204a33615cc6c45daf40481
Michael Ilseman <michael.ilseman@gmail.com>
2021-11-02 11:00:21 -0600

    MatchingEngine takes ranges (#34)

--- Sources/MatchingEngine/Consume.swift
+++ Sources/MatchingEngine/Consume.swift
@@ -1,16 +1,24 @@
 var checkComments = true
 
 extension Engine {
-  func createProcessor(_ input: Input) -> Processor<Input> {
-    Processor(program, input, enableTracing: enableTracing)
+  func createProcessor(
+    _ input: Input, in r: Range<Input.Index>
+  ) -> Processor<Input> {
+    Processor(
+      program, input, in: r, enableTracing: enableTracing)
   }
 
   public func consume(_ input: Input) -> Input.Index? {
+    consume(input, in: input.startIndex ..< input.endIndex)
+  }
+
+  public func consume(
+    _ input: Input, in range: Range<Input.Index>) -> Input.Index? {
     if enableTracing {
       print("Consume: \(input)")
     }
 
-    var cpu = createProcessor(input)
+    var cpu = createProcessor(input, in: range)
     let result: Input.Index? = {
       while true {
         switch cpu.state {
