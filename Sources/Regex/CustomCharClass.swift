a2deb33a0c79256592ab7d4b4c6b7e5ff6ca2c6a
Hamish Knight <hamish_github@mediocremail.com>
2021-11-23 19:32:59 +0000

    Character class mapping for CustomCharacterClass

--- Sources/Regex/CustomCharClass.swift
+++ Sources/Regex/CustomCharClass.swift
@@ -27,3 +27,33 @@ public struct CustomCharacterClass: Hashable {
     case inverted
   }
 }
+
+extension CustomCharacterClass {
+  /// The model character class for this custom character class.
+  var modelCharacterClass: CharacterClass {
+    typealias Component = CharacterClass.CharacterSetComponent
+    func getComponents(_ members: [Member]) -> [Component] {
+      members.map { m in
+        switch m {
+        case .custom(let cc):
+          return .characterClass(cc.modelCharacterClass)
+        case .range(let lhs, let rhs):
+          return .range(
+            lhs.literalCharacterValue! ... rhs.literalCharacterValue!
+          )
+        case .atom(let a):
+          return .characterClass(a.characterClass!)
+        case .setOperation(let lhs, let op, let rhs):
+          // FIXME: CharacterClass wasn't designed for set operations with
+          // multiple components in each operand, we should fix that. For now,
+          // just produce custom components.
+          return .setOperation(
+            .init(lhs: .characterClass(.custom(getComponents(lhs))), op: op,
+                  rhs: .characterClass(.custom(getComponents(rhs))))
+          )
+        }
+      }
+    }
+    return .custom(getComponents(members))
+  }
+}
