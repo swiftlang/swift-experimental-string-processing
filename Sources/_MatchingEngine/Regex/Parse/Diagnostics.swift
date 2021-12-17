b7b36cdb6f98552633122b03942be692b9939b13
Michael Ilseman <michael.ilseman@gmail.com>
2021-12-16 19:41:55 -0700

    CustomStringConvertible for Parser errors (#85)

--- Sources/_MatchingEngine/Regex/Parse/Diagnostics.swift
+++ Sources/_MatchingEngine/Regex/Parse/Diagnostics.swift
@@ -26,6 +26,41 @@ enum ParseError: Error, Hashable {
   case emptyProperty
 }
 
+extension ParseError: CustomStringConvertible {
+  var description: String {
+    switch self {
+    case let .numberOverflow(s):
+      return "number overflow: \(s)"
+    case let .expectedNumDigits(s, i):
+      return "expected \(i) digits in '\(s)'"
+    case let .expectedNumber(s, kind: kind):
+      let radix: String
+      if kind == .decimal {
+        radix = ""
+      } else {
+        radix = " of radix \(kind.radix)"
+      }
+      return "expected a numbers in '\(s)'\(radix)"
+    case let .expected(s):
+      return "expected '\(s)'"
+    case .unexpectedEndOfInput:
+      return "unexpected end of input"
+    case let .misc(s):
+      return s
+    case let .expectedASCII(c):
+      return "expected ASCII for '\(c)'"
+    case .expectedCustomCharacterClassMembers:
+      return "expected custom character class members"
+    case .invalidCharacterClassRangeOperand:
+      return "invalid character class range"
+    case let .invalidPOSIXSetName(n):
+      return "invalid character set name: '\(n)'"
+    case .emptyProperty:
+      return "empty property"
+    }
+  }
+}
+
 // TODO: Fixits, notes, etc.
 
 // TODO: Diagnostics engine, recorder, logger, or similar.
