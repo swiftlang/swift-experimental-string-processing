d9a8c506276f8a0cd70df82fb198993dccacb35b
Nate Cook <natecook@apple.com>
2022-02-01 22:11:39 -0600

    Slim things down a bit to test out the macOS CI issue

--- Package.swift
+++ Package.swift
@@ -6,19 +6,6 @@ import PackageDescription
 let package = Package(
     name: "swift-experimental-string-processing",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "_StringProcessing",
-            targets: ["_StringProcessing"]),
-        .library(
-            name: "Prototypes",
-            targets: ["Prototypes"]),
-        .library(
-            name: "_MatchingEngine",
-            targets: ["_MatchingEngine"]),
-        .library(
-            name: "_Unicode",
-            targets: ["_Unicode"]),
         .executable(
             name: "VariadicsGenerator",
             targets: ["VariadicsGenerator"])
@@ -27,56 +14,17 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "_MatchingEngine",
-            dependencies: [/*"_Unicode"*/],
-            swiftSettings: [
-                .unsafeFlags(["-enable-library-evolution"])
-            ]),
-        .testTarget(
-            name: "MatchingEngineTests",
-            dependencies: ["_MatchingEngine"]),
-        .target(
-            name: "_StringProcessing",
-            dependencies: ["_MatchingEngine"],
-            swiftSettings: [
-                .unsafeFlags(["-enable-library-evolution"])
-            ]),
         .target(
             name: "_Unicode",
             dependencies: []),
         .testTarget(
-            name: "RegexTests",
-            dependencies: ["_StringProcessing"]),
-        .target(
-            name: "Prototypes",
-            dependencies: ["_MatchingEngine"]),
-        .testTarget(
           name: "UnicodeTests",
           dependencies: ["_Unicode"]),
-
-        // MARK: Scripts
         .executableTarget(
             name: "VariadicsGenerator",
             dependencies: [
               .product(name: "ArgumentParser", package: "swift-argument-parser")
             ]),
-        .executableTarget(
-            name: "PatternConverter",
-            dependencies: [
-              .product(name: "ArgumentParser", package: "swift-argument-parser"),
-              "_MatchingEngine",
-            ]),
-
-        // MARK: Exercises
-        .target(
-          name: "Exercises",
-          dependencies: ["_MatchingEngine", "Prototypes", "_StringProcessing"]),
-        .testTarget(
-          name: "ExercisesTests",
-          dependencies: ["Exercises"]),
     ]
 )
 
