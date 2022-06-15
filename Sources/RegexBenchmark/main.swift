10ec7698401d3f9b70298c59f165b136c979f85b
Lily Lin <lily_lin2@apple.com>
2022-06-15 14:29:17 -0700

    Add basic CLI

--- Sources/RegexBenchmark/main.swift
+++
@@ -1,9 +0,0 @@
-
-// todo: make this a proper arg parsing main
-// todo: add an option to run an individual benchmark once for profiling purposes
-var benchmark = BenchmarkRunner(suiteName: "test benchmark")
-benchmark.addReluctantQuant()
-benchmark.addBacktracking()
-benchmark.addCSS()
-benchmark.addFirstMatch()
-benchmark.run()
