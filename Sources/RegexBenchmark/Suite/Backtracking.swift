674c2b74d2a381665ef156771ff6b965bb9b392f
Lily Lin <lily_lin2@apple.com>
2022-06-15 11:03:13 -0700

    Remove top level code
    top level code is real weird, let's not talk about it

--- Sources/RegexBenchmark/Suite/Backtracking.swift
+++ Sources/RegexBenchmark/Suite/Backtracking.swift
@@ -6,25 +6,40 @@ import Foundation
 
 extension BenchmarkRunner {
   mutating func addBacktracking() {
+    let r = "^ +A"
+    let s = String(repeating: " ", count: 1000)
+
+    let basicBacktrack = Benchmark(
+      name: "BasicBacktrack",
+      regex: try! Regex(r),
+      ty: .enumerate,
+      target: s
+    )
+
+    let basicBacktrackNS = NSBenchmark(
+      name: "BasicBacktrackNS",
+      regex: try! NSRegularExpression(pattern: r),
+      ty: .all,
+      target: s
+    )
+
+    let basicBacktrackFirstMatch = Benchmark(
+      name: "BasicBacktrackFirstMatch",
+      regex: try! Regex(r),
+      ty: .first,
+      target: s
+    )
+
+    let basicBacktrackNSFirstMatch = NSBenchmark(
+      name: "BasicBacktrackNSFirstMatch",
+      regex: try! NSRegularExpression(pattern: r),
+      ty: .first,
+      target: s
+    )
+    
     register(new: basicBacktrack)
     register(new: basicBacktrackNS)
+    register(new: basicBacktrackFirstMatch)
+    register(new: basicBacktrackNSFirstMatch)
   }
 }
-
-
-private let r = " +A"
-private let s = String(repeating: " ", count: 100)
-
-private let basicBacktrack = Benchmark(
-  name: "BasicBacktrack",
-  regex: try! Regex(r),
-  ty: .enumerate,
-  target: s
-)
-
-private let basicBacktrackNS = NSBenchmark(
-  name: "BasicBacktrackNS",
-  regex: try! NSRegularExpression(pattern: r),
-  ty: .all,
-  target: s
-)
---
+++ Sources/RegexBenchmark/Suite/FirstMatch.swift
@@ -0,0 +1,25 @@
+import _StringProcessing
+import Foundation
+
+extension BenchmarkRunner {
+  mutating func addFirstMatch() {
+    let r = "a"
+    let s = String(repeating: " ", count: 5000)
+    
+    let firstMatch = Benchmark(
+      name: "FirstMatch",
+      regex: try! Regex(r),
+      ty: .first,
+      target: s
+    )
+    let firstMatchNS = NSBenchmark(
+      name: "FirstMatchNS",
+      regex: try! NSRegularExpression(pattern: r),
+      ty: .first,
+      target: s
+    )
+    
+    register(new: firstMatch)
+    register(new: firstMatchNS)
+  }
+}
