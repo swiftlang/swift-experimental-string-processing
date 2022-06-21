62510530f8be3342e0aecd1be0d0005e8a43f3aa
Michael Ilseman <michael.ilseman@gmail.com>
2022-06-21 11:46:57 -0600

    Remove unused regsiters, opodes (#506)

--- Sources/_StringProcessing/Engine/MEProgram.swift
+++ Sources/_StringProcessing/Engine/MEProgram.swift
@@ -26,7 +26,6 @@ struct MEProgram {
 
   var staticElements: [Input.Element]
   var staticSequences: [[Input.Element]]
-  var staticStrings: [String]
   var staticConsumeFunctions: [ConsumeFunction]
   var staticAssertionFunctions: [AssertionFunction]
   var staticTransformFunctions: [TransformFunction]
@@ -46,7 +45,6 @@ extension MEProgram: CustomStringConvertible {
   var description: String {
     var result = """
     Elements: \(staticElements)
-    Strings: \(staticStrings)
 
     """
     if !staticConsumeFunctions.isEmpty {
@@ -58,9 +56,6 @@ extension MEProgram: CustomStringConvertible {
     for idx in instructions.indices {
       let inst = instructions[idx]
       result += "[\(idx.rawValue)] \(inst)"
-      if let sp = inst.stringRegister {
-        result += " // \(staticStrings[sp.rawValue])"
-      }
       if let ia = inst.instructionAddress {
         result += " // \(instructions[ia])"
       }
