e0af63914f2609966bd50d3aaddbc4e98bcd904e
Lily <rctcwyvrn@gmail.com>
2022-06-21 08:56:05 -0700

    Add more benchmarks and benchmarker functionality (#505)
    * Add debug mode
    
    * Fix typo in css regex
    
    * Add HTML benchmark
    
    * Add email regex benchmarks
    
    * Add save/compare functionality to the benchmarker
    
    * Clean up compare and add cli flags

--- Sources/RegexBenchmark/CLI.swift
+++ Sources/RegexBenchmark/CLI.swift
@@ -5,30 +5,53 @@ struct Runner: ParsableCommand {
   @Argument(help: "Names of benchmarks to run")
   var specificBenchmarks: [String] = []
   
-  @Option(help: "Run only once for profiling purposes")
+  @Flag(help: "Run only once for profiling purposes")
   var profile = false
   
   @Option(help: "How many samples to collect for each benchmark")
   var samples = 20
+  
+  @Flag(help: "Debug benchmark regexes")
+  var debug = false
+  
+  @Option(help: "Output folder")
+  var outputPath = "./results/"
+  
+  @Flag(help: "Should the results be saved")
+  var save = false
+  
+  @Flag(help: "Compare this result with the latest saved result")
+  var compare = false
     
   func makeRunner() -> BenchmarkRunner {
-    var benchmark = BenchmarkRunner("RegexBench", samples)
+    var benchmark = BenchmarkRunner("RegexBench", samples, outputPath)
     benchmark.addReluctantQuant()
     benchmark.addCSS()
     benchmark.addNotFound()
     benchmark.addGraphemeBreak()
     benchmark.addHangulSyllable()
+    benchmark.addHTML()
+    benchmark.addEmail()
     return benchmark
   }
+  
   mutating func run() throws {
     var runner = makeRunner()
     if !self.specificBenchmarks.isEmpty {
       runner.suite = runner.suite.filter { b in specificBenchmarks.contains(b.name) }
     }
-    if profile {
-      runner.profile()
-    } else {
+    switch (profile, debug) {
+    case (true, true): print("Cannot run both profile and debug")
+    case (true, false): runner.profile()
+    case (false, true): runner.debug()
+    case (false, false):
       runner.run()
+      if compare {
+        try runner.compare()
+      }
+      if save {
+        try runner.save()
+      }
     }
   }
 }
---
+++ Utils/generateEmails.py
@@ -0,0 +1,22 @@
+import string
+import random
+
+domain_charset = string.ascii_letters + string.digits + ".-"
+locale_charset = domain_charset + "_%+"
+
+n = 1000
+
+# for the most part this will generate mostly valid emails
+# there are some edge cases with double hyphens and double periods that cause
+# issues but otherwise this should work
+
+for _ in range(n):
+  domain_len = random.randint(2,64)
+  locale_len = random.randint(2,64)
+  tld_len = random.randint(2,10)
+  
+  domain = "".join(random.sample(domain_charset, domain_len))
+  locale = "".join(random.sample(locale_charset, locale_len))
+  tld = "".join(random.sample(string.ascii_lowercase, tld_len))
+  email = locale + "@" + domain + "." + tld
+  print(email.lower())
