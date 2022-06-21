bb558ea253faf91db8625a0d65f3224deebe9f5d
Michael Ilseman <michael.ilseman@gmail.com>
2022-06-21 10:29:03 -0600

    Revert "Add more benchmarks and benchmarker functionality (#505)" (#507)
    This reverts commit e0af63914f2609966bd50d3aaddbc4e98bcd904e.

--- Sources/RegexBenchmark/CLI.swift
+++ Sources/RegexBenchmark/CLI.swift
@@ -5,53 +5,30 @@ struct Runner: ParsableCommand {
   @Argument(help: "Names of benchmarks to run")
   var specificBenchmarks: [String] = []
   
-  @Flag(help: "Run only once for profiling purposes")
+  @Option(help: "Run only once for profiling purposes")
   var profile = false
   
   @Option(help: "How many samples to collect for each benchmark")
   var samples = 20
-  
-  @Flag(help: "Debug benchmark regexes")
-  var debug = false
-  
-  @Option(help: "Output folder")
-  var outputPath = "./results/"
-  
-  @Flag(help: "Should the results be saved")
-  var save = false
-  
-  @Flag(help: "Compare this result with the latest saved result")
-  var compare = false
     
   func makeRunner() -> BenchmarkRunner {
-    var benchmark = BenchmarkRunner("RegexBench", samples, outputPath)
+    var benchmark = BenchmarkRunner("RegexBench", samples)
     benchmark.addReluctantQuant()
     benchmark.addCSS()
     benchmark.addNotFound()
     benchmark.addGraphemeBreak()
     benchmark.addHangulSyllable()
-    benchmark.addHTML()
-    benchmark.addEmail()
     return benchmark
   }
-  
   mutating func run() throws {
     var runner = makeRunner()
     if !self.specificBenchmarks.isEmpty {
       runner.suite = runner.suite.filter { b in specificBenchmarks.contains(b.name) }
     }
-    switch (profile, debug) {
-    case (true, true): print("Cannot run both profile and debug")
-    case (true, false): runner.profile()
-    case (false, true): runner.debug()
-    case (false, false):
+    if profile {
+      runner.profile()
+    } else {
       runner.run()
-      if compare {
-        try runner.compare()
-      }
-      if save {
-        try runner.save()
-      }
     }
   }
 }
