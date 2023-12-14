2d7a3914acd744e64ed65929a6b6fba985b815a4
Michael Ilseman <michael.ilseman@gmail.com>
2023-12-13 20:07:40 -0700

    wip

--- Sources/_StringProcessing/Engine/MEQuantify.swift
+++ Sources/_StringProcessing/Engine/MEQuantify.swift
@@ -9,49 +9,120 @@ extension Processor {
     let produceSavePointRange = payload.quantKind == .eager
     let isScalarSemantics = payload.isScalarSemantics
 
+    let isZeroOrMore = payload.minTrips == 0 && payload.maxExtraTrips == nil
+    let isOneOrMore = payload.minTrips == 1 && payload.maxExtraTrips == nil
+
     let matchResult: (next: String.Index, savePointRange: Range<Position>?)?
     switch payload.type {
     case .asciiBitset:
-      matchResult = input.matchQuantifiedASCIIBitset(
-        registers[payload.bitset],
-        at: currentPosition,
-        limitedBy: end,
-        minMatches: minMatches,
-        maxMatches: maxMatches,
-        produceSavePointRange: produceSavePointRange,
-        isScalarSemantics: isScalarSemantics)
+      if isZeroOrMore {
+        matchResult = input.matchZeroOrMoreASCIIBitset(
+          registers[payload.bitset],
+          at: currentPosition,
+          limitedBy: end,
+          produceSavePointRange: produceSavePointRange,
+          isScalarSemantics: isScalarSemantics)
+      } else if isOneOrMore {
+        matchResult = input.matchOneOrMoreASCIIBitset(
+          registers[payload.bitset],
+          at: currentPosition,
+          limitedBy: end,
+          produceSavePointRange: produceSavePointRange,
+          isScalarSemantics: isScalarSemantics)
+      } else {
+        matchResult = input.matchQuantifiedASCIIBitset(
+          registers[payload.bitset],
+          at: currentPosition,
+          limitedBy: end,
+          minMatches: minMatches,
+          maxMatches: maxMatches,
+          produceSavePointRange: produceSavePointRange,
+          isScalarSemantics: isScalarSemantics)
+      }
 
     case .asciiChar:
-      matchResult = input.matchQuantifiedScalar(
-        Unicode.Scalar(payload.asciiChar),
-        at: currentPosition,
-        limitedBy: end,
-        minMatches: minMatches,
-        maxMatches: maxMatches,
-        produceSavePointRange: produceSavePointRange,
-        isScalarSemantics: isScalarSemantics)
+      if isZeroOrMore {
+        matchResult = input.matchZeroOrMoreScalar(
+          Unicode.Scalar(payload.asciiChar),
+          at: currentPosition,
+          limitedBy: end,
+          produceSavePointRange: produceSavePointRange,
+          isScalarSemantics: isScalarSemantics)
+      } else if isOneOrMore {
+        matchResult = input.matchOneOrMoreScalar(
+          Unicode.Scalar(payload.asciiChar),
+          at: currentPosition,
+          limitedBy: end,
+          produceSavePointRange: produceSavePointRange,
+          isScalarSemantics: isScalarSemantics)
+      } else {
+        matchResult = input.matchQuantifiedScalar(
+          Unicode.Scalar(payload.asciiChar),
+          at: currentPosition,
+          limitedBy: end,
+          minMatches: minMatches,
+          maxMatches: maxMatches,
+          produceSavePointRange: produceSavePointRange,
+          isScalarSemantics: isScalarSemantics)
+      }
 
     case .any:
-      matchResult = input.matchQuantifiedRegexDot(
-        at: currentPosition,
-        limitedBy: end,
-        minMatches: minMatches,
-        maxMatches: maxMatches,
-        produceSavePointRange: produceSavePointRange,
-        isScalarSemantics: isScalarSemantics,
-        anyMatchesNewline: payload.anyMatchesNewline)
+      if isZeroOrMore {
+        matchResult = input.matchZeroOrMoreRegexDot(
+          at: currentPosition,
+          limitedBy: end,
+          produceSavePointRange: produceSavePointRange,
+          anyMatchesNewline: payload.anyMatchesNewline,
+          isScalarSemantics: isScalarSemantics)
+      } else if isOneOrMore {
+        matchResult = input.matchOneOrMoreRegexDot(
+          at: currentPosition,
+          limitedBy: end,
+          produceSavePointRange: produceSavePointRange,
+          anyMatchesNewline: payload.anyMatchesNewline,
+          isScalarSemantics: isScalarSemantics)
+      } else {
+        matchResult = input.matchQuantifiedRegexDot(
+          at: currentPosition,
+          limitedBy: end,
+          minMatches: minMatches,
+          maxMatches: maxMatches,
+          produceSavePointRange: produceSavePointRange,
+          anyMatchesNewline: payload.anyMatchesNewline,
+          isScalarSemantics: isScalarSemantics)
+      }
 
     case .builtin:
-      matchResult = input.matchQuantifiedBuiltinCC(
-        payload.builtin,
-        at: currentPosition,
-        limitedBy: end,
-        minMatches: minMatches,
-        maxMatches: maxMatches,
-        produceSavePointRange: produceSavePointRange,
-        isInverted: payload.builtinIsInverted,
-        isStrictASCII: payload.builtinIsStrict,
-        isScalarSemantics: isScalarSemantics)
+      if isZeroOrMore {
+        matchResult = input.matchZeroOrMoreBuiltinCC(
+          payload.builtin,
+          at: currentPosition,
+          limitedBy: end,
+          produceSavePointRange: produceSavePointRange,
+          isInverted: payload.builtinIsInverted,
+          isStrictASCII: payload.builtinIsStrict,
+          isScalarSemantics: isScalarSemantics)
+      } else if isOneOrMore {
+        matchResult = input.matchOneOrMoreBuiltinCC(
+          payload.builtin,
+          at: currentPosition,
+          limitedBy: end,
+          produceSavePointRange: produceSavePointRange,
+          isInverted: payload.builtinIsInverted,
+          isStrictASCII: payload.builtinIsStrict,
+          isScalarSemantics: isScalarSemantics)
+      } else {
+        matchResult = input.matchQuantifiedBuiltinCC(
+          payload.builtin,
+          at: currentPosition,
+          limitedBy: end,
+          minMatches: minMatches,
+          maxMatches: maxMatches,
+          produceSavePointRange: produceSavePointRange,
+          isInverted: payload.builtinIsInverted,
+          isStrictASCII: payload.builtinIsStrict,
+          isScalarSemantics: isScalarSemantics)
+      }
     }
 
     guard let (next, savePointRange) = matchResult else {
@@ -121,7 +192,55 @@ extension String {
     // position, because newline-sequence in scalar semantic mode still
     // matches two scalars
 
-    return (currentPosition, rangeStart..<rangeEnd)    
+    return (currentPosition, rangeStart..<rangeEnd)
+  }
+
+  /// NOTE: [Zero|One]OrMore overloads are to specialize the inlined run loop,
+  /// which has a substantive perf impact (especially for zero-or-more)
+
+  fileprivate func matchZeroOrMoreASCIIBitset(
+    _ asciiBitset: ASCIIBitset,
+    at currentPosition: Index,
+    limitedBy end: Index,
+    produceSavePointRange: Bool,
+    isScalarSemantics: Bool
+  ) -> (next: Index, savePointRange: Range<Index>?)? {
+    _runQuantLoop(
+      at: currentPosition,
+      limitedBy: end,
+      minMatches: 0,
+      maxMatches: UInt64.max,
+      produceSavePointRange: produceSavePointRange,
+      isScalarSemantics: isScalarSemantics
+    ) { currentPosition, end, isScalarSemantics in
+      matchASCIIBitset(
+        asciiBitset,
+        at: currentPosition,
+        limitedBy: end,
+        isScalarSemantics: isScalarSemantics)
+    }
+  }
+  fileprivate func matchOneOrMoreASCIIBitset(
+    _ asciiBitset: ASCIIBitset,
+    at currentPosition: Index,
+    limitedBy end: Index,
+    produceSavePointRange: Bool,
+    isScalarSemantics: Bool
+  ) -> (next: Index, savePointRange: Range<Index>?)? {
+    _runQuantLoop(
+      at: currentPosition,
+      limitedBy: end,
+      minMatches: 1,
+      maxMatches: UInt64.max,
+      produceSavePointRange: produceSavePointRange,
+      isScalarSemantics: isScalarSemantics
+    ) { currentPosition, end, isScalarSemantics in
+      matchASCIIBitset(
+        asciiBitset,
+        at: currentPosition,
+        limitedBy: end,
+        isScalarSemantics: isScalarSemantics)
+    }
   }
 
   fileprivate func matchQuantifiedASCIIBitset(
@@ -149,6 +268,54 @@ extension String {
     }
   }
 
+  fileprivate func matchZeroOrMoreScalar(
+    _ scalar: Unicode.Scalar,
+    at currentPosition: Index,
+    limitedBy end: Index,
+    produceSavePointRange: Bool,
+    isScalarSemantics: Bool
+  ) -> (next: Index, savePointRange: Range<Index>?)? {
+    _runQuantLoop(
+      at: currentPosition,
+      limitedBy: end,
+      minMatches: 0,
+      maxMatches: UInt64.max,
+      produceSavePointRange: produceSavePointRange,
+      isScalarSemantics: isScalarSemantics
+    ) { currentPosition, end, isScalarSemantics in
+      matchScalar(
+        scalar,
+        at: currentPosition,
+        limitedBy: end,
+        boundaryCheck: !isScalarSemantics,
+        isCaseInsensitive: false)
+    }
+  }
+  fileprivate func matchOneOrMoreScalar(
+    _ scalar: Unicode.Scalar,
+    at currentPosition: Index,
+    limitedBy end: Index,
+    produceSavePointRange: Bool,
+    isScalarSemantics: Bool
+  ) -> (next: Index, savePointRange: Range<Index>?)? {
+    _runQuantLoop(
+      at: currentPosition,
+      limitedBy: end,
+      minMatches: 1,
+      maxMatches: UInt64.max,
+      produceSavePointRange: produceSavePointRange,
+      isScalarSemantics: isScalarSemantics
+    ) { currentPosition, end, isScalarSemantics in
+      matchScalar(
+        scalar,
+        at: currentPosition,
+        limitedBy: end,
+        boundaryCheck: !isScalarSemantics,
+        isCaseInsensitive: false)
+
+    }
+  }
+
   fileprivate func matchQuantifiedScalar(
     _ scalar: Unicode.Scalar,
     at currentPosition: Index,
@@ -173,6 +340,59 @@ extension String {
         boundaryCheck: !isScalarSemantics,
         isCaseInsensitive: false)
 
+    }
+  }
+
+  fileprivate func matchZeroOrMoreBuiltinCC(
+    _ builtinCC: _CharacterClassModel.Representation,
+    at currentPosition: Index,
+    limitedBy end: Index,
+    produceSavePointRange: Bool,
+    isInverted: Bool,
+    isStrictASCII: Bool,
+    isScalarSemantics: Bool
+  ) -> (next: Index, savePointRange: Range<Index>?)? {
+    _runQuantLoop(
+      at: currentPosition,
+      limitedBy: end,
+      minMatches: 0,
+      maxMatches: UInt64.max,
+      produceSavePointRange: produceSavePointRange,
+      isScalarSemantics: isScalarSemantics
+    ) { currentPosition, end, isScalarSemantics in
+      matchBuiltinCC(
+        builtinCC,
+        at: currentPosition,
+        limitedBy: end,
+        isInverted: isInverted,
+        isStrictASCII: isStrictASCII,
+        isScalarSemantics: isScalarSemantics)
+    }
+  }
+  fileprivate func matchOneOrMoreBuiltinCC(
+    _ builtinCC: _CharacterClassModel.Representation,
+    at currentPosition: Index,
+    limitedBy end: Index,
+    produceSavePointRange: Bool,
+    isInverted: Bool,
+    isStrictASCII: Bool,
+    isScalarSemantics: Bool
+  ) -> (next: Index, savePointRange: Range<Index>?)? {
+    _runQuantLoop(
+      at: currentPosition,
+      limitedBy: end,
+      minMatches: 1,
+      maxMatches: UInt64.max,
+      produceSavePointRange: produceSavePointRange,
+      isScalarSemantics: isScalarSemantics
+    ) { currentPosition, end, isScalarSemantics in
+      matchBuiltinCC(
+        builtinCC,
+        at: currentPosition,
+        limitedBy: end,
+        isInverted: isInverted,
+        isStrictASCII: isStrictASCII,
+        isScalarSemantics: isScalarSemantics)
     }
   }
 
@@ -205,14 +425,59 @@ extension String {
     }
   }
 
+  fileprivate func matchZeroOrMoreRegexDot(
+    at currentPosition: Index,
+    limitedBy end: Index,
+    produceSavePointRange: Bool,
+    anyMatchesNewline: Bool,
+    isScalarSemantics: Bool
+  ) -> (next: Index, savePointRange: Range<Index>?)? {
+    _runQuantLoop(
+      at: currentPosition,
+      limitedBy: end,
+      minMatches: 0,
+      maxMatches: UInt64.max,
+      produceSavePointRange: produceSavePointRange,
+      isScalarSemantics: isScalarSemantics
+    ) { currentPosition, end, isScalarSemantics in
+      matchRegexDot(
+        at: currentPosition,
+        limitedBy: end,
+        anyMatchesNewline: anyMatchesNewline,
+        isScalarSemantics: isScalarSemantics)
+    }
+  }
+  fileprivate func matchOneOrMoreRegexDot(
+    at currentPosition: Index,
+    limitedBy end: Index,
+    produceSavePointRange: Bool,
+    anyMatchesNewline: Bool,
+    isScalarSemantics: Bool
+  ) -> (next: Index, savePointRange: Range<Index>?)? {
+    _runQuantLoop(
+      at: currentPosition,
+      limitedBy: end,
+      minMatches: 1,
+      maxMatches: UInt64.max,
+      produceSavePointRange: produceSavePointRange,
+      isScalarSemantics: isScalarSemantics
+    ) { currentPosition, end, isScalarSemantics in
+      matchRegexDot(
+        at: currentPosition,
+        limitedBy: end,
+        anyMatchesNewline: anyMatchesNewline,
+        isScalarSemantics: isScalarSemantics)
+    }
+  }
+
   fileprivate func matchQuantifiedRegexDot(
     at currentPosition: Index,
     limitedBy end: Index,
     minMatches: UInt64,
     maxMatches: UInt64,
     produceSavePointRange: Bool,
-    isScalarSemantics: Bool,
-    anyMatchesNewline: Bool
+    anyMatchesNewline: Bool,
+    isScalarSemantics: Bool
   ) -> (next: Index, savePointRange: Range<Index>?)? {
     _runQuantLoop(
       at: currentPosition,
