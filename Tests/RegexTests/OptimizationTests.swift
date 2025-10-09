c6fad9d0dd61c1c22e9b29bd86b298c294180829
Nate Cook <natecook@apple.com>
2025-10-09 11:48:01 -0500

    Eliminate some no longer relevant tests

--- Tests/RegexTests/OptimizationTests.swift
+++ Tests/RegexTests/OptimizationTests.swift
@@ -15,47 +15,19 @@ import Testing
 
 @Suite struct OptimizationTests {
   @available(macOS 9999, *)
-  @Test(arguments: [#/a/#, #/a+/#, #/(?:a+)/#, #/(?:a)+/#, #/(?m)a+/#, #/ab?c/#, #/(?:a+)+$/#])
+  @Test(arguments: [#/a/#, #/a+/#, #/(?:a+)/#, #/(?:a)+/#, #/(?m)a+/#, #/ab?c/#, #/(?:a+)+$/#, #/(?:(?:a+b)+b)/#])
   func requiredFirstAtom(pattern: Regex<Substring>) throws {
-    let atom = pattern.root.requiredFirstAtom()
-    #expect(atom?.literalCharacterValue == "a", "Missing first character atom in '\(pattern._literalPattern!)'")
-    
     let list = DSLList(tree: pattern.program.tree)
-    var nodes = list.nodes[...]
-    let atom2 = _StringProcessing.requiredFirstAtom(&nodes)
-    #expect(atom2?.literalCharacterValue == "a", "Missing first character atom in '\(pattern._literalPattern!)'")
-    
-    let atom3 = list.requiredFirstAtom()
-    #expect(atom3?.literalCharacterValue == "a", "Missing first character atom in '\(pattern._literalPattern!)'")
+    let atom = list.requiredFirstAtom()
+    #expect(atom?.literalCharacterValue == "a", "Missing first character atom in '\(pattern._literalPattern!)'")
   }
   
   @available(macOS 9999, *)
   @Test(arguments: [#/a?/#, #/(?:a|b)/#, #/[a]/#, #/a?bc/#])
   func noRequiredFirstAtom(pattern: Regex<Substring>) throws {
-    let atom = pattern.root.requiredFirstAtom()
-    #expect(atom == nil, "Unexpected required first atom in '\(pattern._literalPattern!)'")
-    
     let list = DSLList(tree: pattern.program.tree)
-    var nodes = list.nodes[...]
-    let atom2 = _StringProcessing.requiredFirstAtom(&nodes)
-    #expect(atom2 == nil, "Unexpected required first atom in '\(pattern._literalPattern!)'")
-    
-    let atom3 = list.requiredFirstAtom()
-    #expect(atom3 == nil, "Unexpected required first atom in '\(pattern._literalPattern!)'")
-  }
-  
-  @available(macOS 9999, *)
-  @Test(arguments: [#/a/#, #/a+/#, #/(?:a+)/#, #/(?:a)+/#, #/(?m)a+/#, #/cb?a/#])
-  func requiredLastAtom(pattern: Regex<Substring>) throws {
-    let atom = pattern.root.requiredLastAtom()
-    #expect(atom?.literalCharacterValue == "a", "Missing last character atom in '\(pattern._literalPattern!)'")
-  }
-  
-  @available(macOS 9999, *)
-  @Test(arguments: [#/a?/#, #/a*/#, #/(?:a|b)/#, #/[a]/#, #/abc?/#])
-  func noRequiredLastAtom(pattern: Regex<Substring>) throws {
-    let atom = pattern.root.requiredLastAtom()
-    #expect(atom == nil, "Unexpected required last atom in '\(pattern._literalPattern!)'")
+    let atom = list.requiredFirstAtom()
+    #expect(atom == nil, "Unexpected required first atom in '\(pattern._literalPattern!)'")
   }
   
   @available(macOS 9999, *)
