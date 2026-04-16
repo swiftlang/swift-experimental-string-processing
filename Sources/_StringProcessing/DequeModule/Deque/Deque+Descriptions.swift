b752a54700cf694d37b214d747bdf8c65463a55d
Nate Cook <natecook@apple.com>
2026-04-16 12:34:08 -0500

    Switch to using a deque for DSLList storage

---
+++ Sources/_StringProcessing/DequeModule/Deque/Deque+Descriptions.swift
@@ -0,0 +1,28 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Collections open source project
+//
+// Copyright (c) 2021 - 2026 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0 WITH Swift-exception
+//
+//===----------------------------------------------------------------------===//
+
+#if !$Embedded
+extension Deque: CustomStringConvertible {
+  /// A textual representation of this instance.
+  public var description: String {
+    "" //_arrayDescription(for: self)
+  }
+}
+
+extension Deque: CustomDebugStringConvertible {
+  /// A textual representation of this instance, suitable for debugging.
+  public var debugDescription: String {
+    description
+  }
+}
+#endif
